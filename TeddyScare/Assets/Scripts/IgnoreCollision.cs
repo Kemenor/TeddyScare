352429b312c952feac7ee34291331665299eb247
Thomas Kunkel <Thomas.Kunkel@students.fhnw.ch>
2017-06-08 12:30:22 +0200

    fixed tree collision and removed Stone.png

--- TeddyScare/Assets/Scripts/IgnoreCollision.cs
+++ TeddyScare/Assets/Scripts/IgnoreCollision.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IgnoreCollision : MonoBehaviour {
-    [SerializeField]
-    private Collider2D other;
+public class IgnoreCollision : MonoBehaviour
+{
+	[SerializeField]
+	private Collider2D other;
+	[SerializeField]
+	private Collider2D[] moreIgnore;
 	// Use this for initialization
-	private void Awake () {
-        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), other, true);
+	private void Awake()
+	{
+		Physics2D.IgnoreCollision(GetComponent<Collider2D>(), other, true);
+		foreach (var item in moreIgnore)
+		{
+			Physics2D.IgnoreCollision(item, other, true);
+		}
 	}
 }
