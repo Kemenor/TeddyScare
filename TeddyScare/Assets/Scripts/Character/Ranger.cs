b84db8af34ec75079a4f2d1925c23a46458b10f1
Thomas Kunkel <Thomas.Kunkel@students.fhnw.ch>
2017-06-08 11:23:34 +0200

    fixed ranger

--- TeddyScare/Assets/Scripts/Character/Ranger.cs
+++ TeddyScare/Assets/Scripts/Character/Ranger.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 
 public class Ranger : StateEnemy
 {
-    public override void Reset()
-    {
-        base.Reset();
-        ChangeState(new RangerIdleState());
-    }
-    
+	public override void Start()
+	{
+		base.Start();
+		ChangeState(new RangerIdleState());
+	}
+	public override void Reset()
+	{
+		base.Reset();
+		ChangeState(new RangerIdleState());
+	}
 
-    public void ShootPlayer()
-    {
-        //Los check kill player
-        if (LineOfSightToPlayer(2))
-        {
-            //kill player
-            Game.Instance.KillPlayer();
-        }
-        else
-        {
-            ChangeState(new RangerPatrolState());
-        }
 
-    }
+	public void ShootPlayer()
+	{
+		//Los check kill player
+		if (LineOfSightToPlayer(2))
+		{
+			//kill player
+			Game.Instance.KillPlayer();
+		}
+		else
+		{
+			ChangeState(new RangerPatrolState());
+		}
+
+	}
 }
--- TeddyScare/Assets/Scripts/StateEnemy.cs
+++ TeddyScare/Assets/Scripts/StateEnemy.cs
@@ -2,37 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class StateEnemy : Enemy {
-    protected IEnemyState currentState;
-    // Use this for initialization
-    // Use this for initialization
-    public override void Start()
-    {
-        base.Start();
-    }
+public abstract class StateEnemy : Enemy
+{
+	[SerializeField]
+	protected IEnemyState currentState;
+	// Use this for initialization
+	// Use this for initialization
+	public override void Start()
+	{
+		base.Start();
+	}
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (currentState != null)
-        {
-            currentState.Execute();
-        }
-    }
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if (currentState != null)
+		{
+			currentState.Execute();
+		}
+	}
 
-    protected void OnTriggerEnter2D(Collider2D collision)
-    {
-        currentState.OnTriggerenter(collision);
-    }
+	protected void OnTriggerEnter2D(Collider2D collision)
+	{
+		currentState.OnTriggerenter(collision);
+	}
 
-    public void ChangeState(IEnemyState newState)
-    {
-        if (currentState != null)
-        {
-            currentState.Exit();
-        }
+	public void ChangeState(IEnemyState newState)
+	{
+		if (currentState != null)
+		{
+			currentState.Exit();
+		}
 
-        currentState = newState;
-        currentState.Enter(this);
-    }
+		currentState = newState;
+		currentState.Enter(this);
+	}
 }
