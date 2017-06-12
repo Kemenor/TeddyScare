501846f5fd923fb39b1a67e5057ab30371f9085e
Dominic Bär <dominic.baer@students.fhnw.ch>
2017-06-12 10:12:40 +0200

    added boar & wolf scripts
    @Kemenor pls check

---
+++ TeddyScare/Assets/Scripts/Character/Boar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boar : StateEnemy
+{
+    public override void Start()
+    {
+        base.Start();
+        ChangeState(new BoarIdleState());
+    }
+    public override void Reset()
+    {
+        base.Reset();
+        ChangeState(new BoarIdleState());
+    }
+
+
+    public void ShootPlayer()
+    {
+        //Los check kill player
+        if (LineOfSightToPlayer(2))
+        {
+            //kill player
+            Level.Instance.KillPlayer();
+        }
+        else
+        {
+            ChangeState(new BoarPatrolState());
+        }
+
+    }
+}
---
+++ TeddyScare/Assets/Scripts/Character/Wolf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wolf : StateEnemy
+{
+    public override void Start()
+    {
+        base.Start();
+        ChangeState(new WolfIdleState());
+    }
+    public override void Reset()
+    {
+        base.Reset();
+        ChangeState(new WolfIdleState());
+    }
+
+
+    public void ShootPlayer()
+    {
+        //Los check kill player
+        if (LineOfSightToPlayer(2))
+        {
+            //kill player
+            Level.Instance.KillPlayer();
+        }
+        else
+        {
+            ChangeState(new WolfPatrolState());
+        }
+
+    }
+}
