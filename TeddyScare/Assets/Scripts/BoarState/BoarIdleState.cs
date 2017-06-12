9749f0c8a6ebb5e20504892f7d4e3c6e9f91ca74
Dominic Bär <dominic.baer@students.fhnw.ch>
2017-06-12 10:04:03 +0200

    add wolf & boar states
    @Kemenor pls check

---
+++ TeddyScare/Assets/Scripts/BoarState/BoarIdleState.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoarIdleState : IEnemyState
+{
+    private StateEnemy enemy;
+
+    private float idleTimer = 0;
+    private float idleDuration = 5f;
+    public void Enter(StateEnemy enemy)
+    {
+        enemy.Anim.SetFloat("Speed", 0f);
+        this.enemy = enemy;
+    }
+
+    public void Execute()
+    {
+        if (enemy.LineOfSightToPlayer())
+        {
+            Level.Instance.AddToDangerLevel();
+            //TODO start hide
+            enemy.gameObject.SetActive(false);
+        }
+        idleTimer += Time.deltaTime;
+        if (idleTimer >= idleDuration)
+        {
+            idleTimer = 0;
+            enemy.ChangeState(new BoarPatrolState());
+            enemy.ChangeDirection();
+        }
+    }
+
+    public void Exit()
+    {
+    }
+
+    public void OnTriggerenter(Collider2D other)
+    {
+    }
+}
